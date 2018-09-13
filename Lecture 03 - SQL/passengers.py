9f9085aa46f2c620a5e6a0f0162e4d4b85139694
granaderos <marejeanperpinosa@gmail.com>
2018-09-13 17:25:23 +0800

    Added feature viewing of passengers.

--- Lecture 03 - SQL/passengers.py
+++ Lecture 03 - SQL/passengers.py
@@ -4,8 +4,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine=create_engine(os.getenv("DATABASE_URL"))
-db=scoped_session(sessionmaker(bind=engine))
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 def main():
 
