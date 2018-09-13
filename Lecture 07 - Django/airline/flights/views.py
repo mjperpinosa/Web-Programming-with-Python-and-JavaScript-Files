1d1e64890c32d95a16ab27f1f0b9552a46a60a01
granaderos <marejeanperpinosa@gmail.com>
2018-09-13 17:12:09 +0800

    Lecture 7: added feature viewing of flights for airline app.

--- Lecture 07 - Django/airline/flights/views.py
+++ Lecture 07 - Django/airline/flights/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
+from .models import Flight
 # Create your views here.
 def index(request):
-    return HttpResponse("Flights")
+    context = {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight does not exist.")
+    context = {
+        "flight": flight
+    }
+    return render(request, "flights/flight.html", context)
