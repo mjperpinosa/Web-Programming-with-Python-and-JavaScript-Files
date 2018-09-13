de72c0a14ebee459bec187c186f930e47665216d
granaderos <marejeanperpinosa@gmail.com>
2018-09-13 17:55:42 +0800

    Added feature booking of flights.

--- Lecture 07 - Django/airline/flights/views.py
+++ Lecture 07 - Django/airline/flights/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 # Create your views here.
 def index(request):
     context = {
@@ -16,6 +17,22 @@ def flight(request, flight_id):
         raise Http404("Flight does not exist.")
     context = {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     }
     return render(request, "flights/flight.html", context)
+
+def book(request, flight_id):
+    try:
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        flight = Flight.objects.get(pk=flight_id)
+    except KeyError:
+        return render(request, "flights/error.html", {"message": "No selection."})
+    except Passenger.DoesNotExist:
+        return render(request, "flights/error.html", {"message": "No passenger."})
+    except Flight.DoesNotExist:
+        return render(request, "flights/error.html", {"message": "No such flight."})
+
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
